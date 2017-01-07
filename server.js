30cd38dc564786069bfed2c4352ecae9bd38120a
Ain <ain.salula@gmail.com>
2017-01-07 03:03:11 +0200

    connect to mongodb

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 import express from 'express';
 
+import {MongoClient} from 'mongodb';
+
 let app = express();
 
 app.get('/', (req, res) => res.send('hello express!'));
 
 app.listen(3000);
+
+MongoClient.connect(process.env.MONGO_URL, (err, database) => {
+    if (err) throw err;
+
+    database.collection("links").find({}).toArray((err, links) => {
+        if (err) throw err;
+
+        console.log(links);            
+    });
+});
